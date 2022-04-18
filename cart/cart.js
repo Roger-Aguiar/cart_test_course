1b507104777359bd3345322dbada406fe7785b71
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-04-18 15:26:49 -0300

    It creates some tests and methods getTotal, add and remove.

--- cart/cart.js
+++ cart/cart.js
@@ -1,51 +1,22 @@
 import { find, remove } from "lodash";
-import Dinero from "dinero.js";
-
-const Money = Dinero;
-Money.defaultCurrency = "BRL";
-Money.defaultPrecision = 2;
 
 export default class Cart {
   items = [];
 
   add(item) {
-    const itemToFind = { product: item.product };
-    if (find(this.items, itemToFind)) {
-      remove(this.items, itemToFind);
+    if (find(this.items, { product: item.product })) {
+      remove(this.items, { product: item.product });
     }
-
     this.items.push(item);
   }
 
-  remove(product) {
-    remove(this.items, product);
-  }
-
   getTotal() {
-    return this.items.reduce((accumulator, item) => {
-      return accumulator.add(
-        Money({ amount: item.quantity * item.product.price })
-      );
-    }, Money({ amount: 0 }));
+    return this.items.reduce((acc, item) => {
+      return acc + item.quantity * item.product.price;
+    }, 0);
   }
 
-  getSummary() {
-    const total = this.getTotal().getAmount();
-    const items = this.items;
-
-    return {
-      total,
-      items,
-    };
-  }
-
-  checkout() {
-    const { total, items } = this.getSummary();
-    this.items = [];
-
-    return {
-      total,
-      items,
-    };
+  remove(product){
+    remove(this.items, {product});
   }
 }
--- cart/cart.spec.js
+++ cart/cart.spec.js
@@ -1,57 +1,62 @@
 import Cart from "./cart";
-import { item, item2, checkoutList, summary } from "../mocks/cart.mock";
 
 describe("Cart", () => {
   let cart;
+  let product = {
+    title: "Adidas running shoes - men",
+    price: 35388,
+  };
 
-  //beforeEach means that before each test to be run, a new instance of Cart will be creted.
+  let product2 = {
+    title: "Adidas running shoes - women",
+    price: 41872,
+  };
+
   beforeEach(() => {
     cart = new Cart();
   });
 
-  describe("getTotal()", () => {
-    it("should return 0 at getTotal when a new instance is creted.", () => {
-      expect(cart.getTotal().getAmount()).toEqual(0);
-    });
-
-    it("should multiply quantity and price and receive the total amount", () => {
-      cart.add(item);
-      expect(cart.getTotal().getAmount()).toBe(70776);
-    });
-
-    it("should ensure that no more than one product exists at a time", () => {
-      cart.add(item);
-      cart.add(item);
-      expect(cart.getTotal().getAmount()).toBe(70776);
-    });
-
-    it("should update total when a product is added or removed.", () => {
-      cart.add(item);
-      cart.add(item2);
-      cart.remove(item.product);
-      expect(cart.getTotal().getAmount()).toBe(112648);
-    });
+  it("Should return 0 when getTotal() is executed in a newly created instance.", () => {
+    expect(cart.getTotal()).toEqual(0);
   });
 
-  describe("checkout", () => {
-    it("should return an object with the total and the list of items", () => {
-      cart.add(item);
-      cart.add(item2);
-      expect(cart.checkout()).toEqual(checkoutList);
+  it("Should multiply quantity and price and receive the total amount.", () => {
+    const item = {
+      product,
+      quantity: 2,
+    };
+
+    cart.add(item);
+    expect(cart.getTotal()).toEqual(70776);
+  });
+
+  it("Should ensure that no more than one product exists at a time.", () => {
+    cart.add({
+      product,
+      quantity: 2,
     });
 
-    it("should reset the cart when checkout is called", () => {
-      cart.add(item2);
-      cart.checkout();
-      expect(cart.getTotal().getAmount()).toEqual(0);
+    cart.add({
+      product,
+      quantity: 1,
     });
 
-    it("should return an object with the total and the list of items", () => {
-      cart.add(item);
-      cart.add(item2);
-      cart.getSummary();
-      expect(cart.getSummary()).toEqual(summary);
-      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
+    expect(cart.getTotal()).toEqual(35388);
+  });
+
+  it("Should update total when a product gets included and then removed", () => {
+    cart.add({
+      product,
+      quantity: 2,
     });
+
+    cart.add({
+      product: product2,
+      quantity: 1,
+    });
+
+    cart.remove(product);
+
+    expect(cart.getTotal()).toEqual(41872);
   });
 });
